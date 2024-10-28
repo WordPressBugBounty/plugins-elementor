a5079a9343a2badd6c2f9e083442c4472caa595a
Sean Murphy <sean@iamseanmurphy.com>
2024-10-28 20:27:44 +0000

    3.25.1

--- elementor/vendor/composer/installed.php
+++ elementor/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'elementor/elementor',
         'pretty_version' => '3.25.x-dev',
         'version' => '3.25.9999999.9999999-dev',
-        'reference' => '5b0b2671cafa6ece08e30f3b54dd94df22fb8a29',
+        'reference' => '37edc3100e1bcff065e69cffa4357aa32f3d1161',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'elementor/elementor' => array(
             'pretty_version' => '3.25.x-dev',
             'version' => '3.25.9999999.9999999-dev',
-            'reference' => '5b0b2671cafa6ece08e30f3b54dd94df22fb8a29',
+            'reference' => '37edc3100e1bcff065e69cffa4357aa32f3d1161',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
